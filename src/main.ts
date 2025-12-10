66cf2fb8fb7cd72215da66e3a957bedd8462b53d
Andrea Vilselius <andrea.vilselius@azeea.com>
2025-12-10 14:39:34 +0100

    tabortskit

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,5 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 //anv geolocation
 //name- origin : plats på id / koordinater
 //stop lookup : https://journeyplanner.integration.sl.se/v2/stop-finder?name_sf=odenplan&any_obj_filter_sf=2&type_sf=any
@@ -29,4 +7,3 @@ setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 //anv stop lookup: visar id för odenplan. destination för url
 
 //https://journeyplanner.integration.sl.se/v2/trips?type_origin=any&type_destination=any&name_origin=9025001000001861&name_destination=9091001000009192&calc_number_of_trips=3
-console.log("hej");
