5bb11beb438831dc027f42db82fcfaba0b5b7031
Andrea Vilselius <andrea.vilselius@azeea.com>
2025-12-10 14:17:52 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -29,3 +29,4 @@ setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 //anv stop lookup: visar id för odenplan. destination för url
 
 //https://journeyplanner.integration.sl.se/v2/trips?type_origin=any&type_destination=any&name_origin=9025001000001861&name_destination=9091001000009192&calc_number_of_trips=3
+console.log("hej");
